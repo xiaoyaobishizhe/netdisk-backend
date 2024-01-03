f1f3bfae2c880caf5501f29568c7cc667b0a9eab
networkxiake <networkxiake@qq.com>
2024-01-03 12:12:44 +0800

    bug fix

--- netdisk-file/src/main/java/com/xiaoyao/netdisk/file/controller/FileController.java
+++ netdisk-file/src/main/java/com/xiaoyao/netdisk/file/controller/FileController.java
@@ -2,6 +2,7 @@ package com.xiaoyao.netdisk.file.controller;
 
 import com.xiaoyao.netdisk.common.exception.R;
 import com.xiaoyao.netdisk.file.service.FileService;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.annotation.Validated;
@@ -21,7 +22,7 @@ public class FileController {
 
     @PutMapping("/folder")
     public R<Void> createFolder(@Pattern(regexp = "^\\d{1,19}$") String parentId,
-                                @Length(min = 1, max = 250) String folderName) {
+                                @NotNull @Length(min = 1, max = 250) String folderName) {
         fileService.createFolder(parentId, folderName);
         return R.ok();
     }
